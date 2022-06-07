73c8b438ebfbe12999fb9b1d486bbd5a1fe2c7f1
Davidferreirajesus <davidguitarreiro2010@gmail.com>
2022-06-07 18:43:32 -0300

    sorteio

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
-
-
+let pessoas = [
+  'Igor',
+  'Jose',
+  'Marcos',
+  'Pamela',
+  'Joana',
+  'David',
+  'Daniela',
+  'Sarah',
+  'Antônia',
+  'Elizinaria'
+]
+function sortear() {
+  let np = pessoas.length
+  let ns = Math.floor(Math.random() * np)
+  document.getElementById('d').innerHTML = pessoas[ns]
+}
